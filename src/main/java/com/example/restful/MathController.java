e7bf1bf92d973daf391d0b1e4aa26da153f9ea36
pedroalcantara9568 <pedroalcantara.info@gmail.com>
2020-05-15 12:38:54 -0300

    alterações diversas

--- src/main/java/com/example/restful/MathController.java
+++ src/main/java/com/example/restful/MathController.java
@@ -1,19 +1,48 @@
 package com.example.restful;
 
 
+import com.example.restful.exception.OperacaoNaoSuportadaException;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MathController {
 
-    @RequestMapping(value = "/soma/{numberOne}/{numberTwo}", method = RequestMethod.GET)
-    public Double greeting(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
-      if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
-        throw new Exception();
-      }
-      Double sum = convertToDouble(numberOne) + convertToDouble(numberTwo);
-      return sum;
+    @RequestMapping(value = "/soma/{numeroUm}/{numeroDois}", method = RequestMethod.GET)
+    public Double soma(@PathVariable("numeroUm") String numeroUm, @PathVariable("numeroDois") String numeroDois) throws OperacaoNaoSuportadaException {
+      if(!isNumeric(numeroUm) || !isNumeric(numeroDois)) throw new OperacaoNaoSuportadaException("por favor defina um valor numérico");
+        return convertToDouble(numeroUm) + convertToDouble(numeroDois);
     }
+    
+    @RequestMapping(value = "/subtrair/{numeroUm}/{numeroDois}" , method = RequestMethod.GET)
+    public Double subtracao(@PathVariable("numeroUm") String numeroUm, @PathVariable("numeroDois") String numeroDois) throws OperacaoNaoSuportadaException {
+        if(!isNumeric(numeroUm) || !isNumeric(numeroDois)) throw new OperacaoNaoSuportadaException("por favor defina um valor numérico");
+        return convertToDouble(numeroUm) - convertToDouble(numeroDois);
+    }
+
+    @RequestMapping(value = "/multiplicar/{numeroUm}/{numeroDois}", method = RequestMethod.GET)
+    public Double multiplicacao(@PathVariable("numeroUm") String numeroUm, @PathVariable("numeroDois") String numeroDois) throws OperacaoNaoSuportadaException {
+        if(!isNumeric(numeroUm) || !isNumeric(numeroDois)) throw new OperacaoNaoSuportadaException("por favor defina um valor numérico");
+        return convertToDouble(numeroUm) * convertToDouble(numeroDois);
+    }
+
+    @RequestMapping(value = "/dividir/{numeroUm}/{numeroDois}", method = RequestMethod.GET)
+    public Double divisao(@PathVariable("numeroUm") String numeroUm, @PathVariable("numeroDois") String numeroDois) throws OperacaoNaoSuportadaException {
+        if(!isNumeric(numeroUm) || isNumeric(numeroDois)) throw new OperacaoNaoSuportadaException("por favor definar um valor numérico");
+        return (convertToDouble(numeroUm) / convertToDouble(numeroDois)) ;
+    }
+
+    @RequestMapping(value = "/media/{numeroUm}/{numeroDois}", method = RequestMethod.GET)
+    public Double media(@PathVariable("numeroUm") String numeroUm, @PathVariable("numeroDois") String numeroDois) throws OperacaoNaoSuportadaException {
+        if(!isNumeric(numeroUm) || isNumeric(numeroDois)) throw new OperacaoNaoSuportadaException("por favor definar um valor numérico");
+        return (convertToDouble(numeroUm) + convertToDouble(numeroDois)) / 2;
+    }
+
+    @RequestMapping(value = "/raiz/{numeroUm}", method = RequestMethod.GET)
+    public Double raizQuadrada(@PathVariable("numeroUm") String numeroUm) throws OperacaoNaoSuportadaException {
+        if(!isNumeric(numeroUm)) throw new OperacaoNaoSuportadaException("por favor defina um valor Numerico");
+        return Math.sqrt(convertToDouble(numeroUm));
+    }
+
 
     private Double convertToDouble(String strNumber) {
         if(strNumber == null) return 0D;
